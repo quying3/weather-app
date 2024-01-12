ac12242eb85f909b1dfcfe475d59edb599122726
larryw <larrywong9898@gmail.com>
2024-01-12 14:15:49 -0600

    create weather forecast app

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+// import "./App.css";
+import Test from "@/components/Test";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="flex flex-row justify-center items-center w-screen min-h-screen border-2 border-orange-500">
+      <h1 className="text-3xl font-bold underline center">Hello world!</h1>
+      <Test />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Test.jsx
@@ -0,0 +1,5 @@
+const Test = () => {
+  return <>Test Component ok</>;
+};
+
+export default Test;
