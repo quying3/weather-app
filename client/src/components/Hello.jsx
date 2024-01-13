c13ab9312344723efb632fbe5a1976952184b457
larryw <larrywong9898@gmail.com>
2024-01-12 21:20:40 -0600

    test lint-staged

--- client/src/components/Hello.jsx
+++ client/src/components/Hello.jsx
@@ -1,9 +1,8 @@
-import React from "react";
-
 const Hello = () => {
   return (
     <>
       <div className="flex flex-col gap-2">
+        <h1>hello</h1>
         <a href="http://127.0.0.1:4000/hello">nodejs click me</a>
         <a href="http://127.0.0.1:8080/hello">springboot click me</a>
       </div>
